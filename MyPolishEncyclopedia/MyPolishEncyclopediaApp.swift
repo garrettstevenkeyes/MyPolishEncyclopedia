b4add65b54124ee04db8343d8f08c73993194f05
Garrett Keyes <garrett.keyes@gmail.com>
2026-04-15 21:45:41 -0400

    Initial Commit

---
+++ MyPolishEncyclopedia/MyPolishEncyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPolishEncyclopediaApp.swift
+//  MyPolishEncyclopedia
+//
+//  Created by Garrett Keyes on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MyPolishEncyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
